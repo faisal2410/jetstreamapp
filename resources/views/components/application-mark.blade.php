4c5cf0f0b55e3ad55b21b2fcc5039daa55a772de
Faisal ahmed <faisal2410@yahoo.com>
2023-07-05 15:13:56 +0600

    initial commit

---
+++ resources/views/components/application-mark.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset('images/lead.jpeg') }}" alt="application logo" width="100px"/>
---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/">
+   <img src="{{ asset('images/lead.jpeg') }}" alt="application logo" width="100px"/>
+</a>
